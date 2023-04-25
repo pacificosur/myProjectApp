ae31fcf5570d6a3ea1261441bc45592a52ef1189
labdessw06 <labdessw06@labdessw06>
2023-04-25 12:43:01 -0600

    Creación del proyecto de POO I.

---
+++ src/controller/Test.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import view.*;
+
+/**
+ *
+ * @author labdessw06
+ */
+public class Test {
+    
+}
